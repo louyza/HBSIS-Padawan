093e41f925c8e065b9e6c8ae3d87cc4af67f77b1
louyza <louyzadut@gmail,com>
2020-01-02 16:03:28 -0300

    Curso Intro Python da Udemy Prof Diego Mariano

---
+++ Udemy_Introducao_Python.py
@@ -0,0 +1,57 @@
+# # idade = int(input("Qual sua idade? "))
+# #
+# # if idade >= 18:
+# #     print("Você é maior de idade")
+# # else:
+# #     print("Você é menor de idade")
+
+# -----------------------------------------
+
+# nota_1 = int(input("Digite a primeira nota: "))
+# nota_2 = int(input("Digite a segunda nota: "))
+#
+# total = (nota_1 + nota_2) / 2
+#
+# if total >= 6:
+#     print("Você foi APROVADO!!!")
+# else:
+#     print("Ih, foi REPROVADO!!!")
+
+# -----------------------------------------
+
+# x = 3
+# y = 8
+# z = 4
+#
+# equacao = (x + y) * y * 2 + (z - x)
+#
+# print(equacao)
+
+# -----------------------------------------
+
+# listinha = [64, 235, 7]
+#
+# listinha.sort()
+#
+# print(listinha)
+
+# -----------------------------------------
+
+# n1 = int(input("Digite um número: "))
+# o = (input("Digite um operador: "))
+# n2 = int(input("Digite outro número: "))
+#
+# if o == "+":
+#     operacao = float(n1) + float(n2)
+#     print(operacao)
+# elif o == "-":
+#     operacao = float(n1) - float(n2)
+#     print(operacao)
+# elif o == "/":
+#     operacao = float(n1) / float(n2)
+#     print(operacao)
+# elif o == "*":
+#     operacao = float(n1) * float(n2)
+#     print(operacao)
+# else:
+#     print("Operação inválida!!!")
