6ea6e41045e3d463e67a21a58e8dc88ef7b0fdf1
Louyza Duarte <louyza.duarte@hbsis.com.br>
2020-01-16 14:43:13 -0300

    Quase

--- desafio_individual_road_detective/app/dabdahda.py
+++ desafio_individual_road_detective/app/dabdahda.py
@@ -17,3 +17,60 @@
 #      if list
 #
 # print(scan)
+
+
+# # ----- variables to add the results -----
+# scans_i = ''.join(scan)  # ----- tranforms the animal list into an string -----
+# scans_position = 0  # ----- gets each position like scans numbers, as while -----
+# animals_position = 0  # gets each position like animals numbers, as for -----
+#
+# # ----- ve se em lista de animais tem scan-----
+# for item in animals:
+#     animals_i = ''.join(animals[animals_position])  # ----- string the list you had in the animal list -----
+#     while scans_position < len(scans_i):
+#         if scan[
+#             scans_position] in animals_i:  # ----- checks whether each letter of the scan is in the strings in the list of animals -----
+#             print(f'{animals_i}: True')
+#         else:
+#             print(False)
+#         scans_position += 1  # add one more to change the scan letter position each time the while runs -----
+#     animals_position += 1  # add one more to change the scan letter position each time the for runs -----
+#     scans_position = 0
+#
+# print(animals[animals_position])
+
+# if scan.intersection(animals[i]):
+#     print(True)
+
+# while A LISTA DE ANIMAIS NÃO TIVER UMA LISTA COM OS MESMOS CARACTERES DA LISTA SCAN:
+#     for i in scan: PEGA O ITEM DE SCAN E COMPARA COM A PRIMEIRA LISTA DA DOS ANIMAIS
+#                     SE TIVER, PASSA PARA O PRÓXIMO E ASSIM POR DIANTE
+#         for item in animals[i]: A LISTA DOS ANIMAIS QUE TIVER  MAIS ITENS IGUAIS AO DA SCAN
+#             if i == item:       PARA ISSO TENHO QUE FAZER UMA FUNÇÃO QUE CRIE VARIÁVEIS COM OS "PONTOS" DE CADA LISTA
+
+
+# search = re.search(scan, animals, re.IGNORECASE)
+# print(search)
+
+
+# for item in animals:
+#   if i in item:
+#       print(True)
+
+
+# def list_contains(List1, List2):
+#     check = False
+#
+#     # Iterate in the 1st list
+#     for m in List1:
+#
+#         # Iterate in the 2nd list
+#         for n in List2:
+#
+#             # if there is a match
+#             if m == n:
+#                 check = True
+#                 return check
+#
+#     return check
+
--- desafio_individual_road_detective/app/road_detective.py
+++ desafio_individual_road_detective/app/road_detective.py
@@ -31,13 +31,8 @@
 #
 #     def scan_files(self):
 
-import re
-
 # ----- files with "animal parts"  -----
 files = input("Choose files to scan: ")
-
-# ----- removing unwanted characters -----
-photo = "".join(files.split("="))
 
 # ----- animals list list -----
 animals = [["a", "a", "r", "d", "v", "a", "r", "k"], ["a", "l", "l", "i", "g", "a", "t", "o", "r"],
@@ -48,85 +43,69 @@ animals = [["a", "a", "r", "d", "v", "a", "r", "k"], ["a", "l", "l", "i", "g", "
            ["d", "o", "l", "p", "h", "i", "n"], ["d", "u", "c", "k"], ["d", "r", "a", "g", "o", "n", "f", "l", "y"],
            ["e", "a", "g", "l", "e"], ["e", "l", "e", "p", "h", "a", "n", "t"], ["e", "m", "u"],
            ["e", "c", "h", "i", "d", "n", "a"], ["f", "i", "s", "h"], ["f", "r", "o", "g"],
-           ["f", "l", "a", "m", "i", "n", "g", "o"], ["f", "o", "x"], ["g, o, a, t"], ["g", "i", "r", "a", "f", "f", "e"],
-           ["g", "i", "b", "b", "o", "n"], ["g", "e", "c", "k", "o"], ["h", "y", "e", "n", "a"],
-           ["h", "i", "p", "p", "o", "p", "o", "t", "a", "m", "u", "s"], ["h", "o", "r", "s", "e"],
-           ["h", "a", "m", "s", "t", "e", "r"], ["i", "n", "s", "e", "c", "t"], ["i", "m", "p", "a", "l", "a"],
-           ["i", "g", "u", "a", "n", "a"], ["i", "b", "i", "s"], ["j", "a", "c", "k", "a", "l"],
-           ["j", "a", "g", "u", "a", "r"], ["j", "e", "l", "l", "y", "f", "i", "s", "h"],
-           ["k", "a", "n", "g", "a", "r", "o", "o"], ["k", "i", "w", "i"], ["k", "o", "a", "l", "a"],
-           ["k", "i", "l", "l", "e", "r", "w", "h", "a", "l", "e"], ["l", "e", "m", "u", "r"],
-           ["l", "e", "o", "p", "a", "r", "d"], ["l", "l", "a", "m", "a"], ["l", "i", "o", "n"],
-           ["m", "o", "n", "k", "e", "y"], ["m", "o", "u", "s", "e"], ["m", "o", "o", "s", "e"],
-           ["m", "e", "e", "r", "c", "a", "t"], ["n", "u", "m", "b", "a", "t"], ["n", "e", "w", "t"],
-           ["o", "s", "t", "r", "i", "c", "h"], ["o", "t", "t", "e", "r"], ["o", "c", "t", "o", "p", "u", "s"],
-           ["o", "r", "a", "n", "g", "u", "t", "a", "n"], ["p", "e", "n", "g", "u", "i", "n"],
-           ["p", "a", "n", "t", "h", "e", "r"], ["p", "a", "r", "r", "o", "t"], ["p", "i", "g"], ["q", "u", "a", "i", "l"],
-           ["q", "u", "o", "k", "k", "a"], ["q", "u", "o", "l", "l"], ["r", "a", "t"],
-           ["r", "h", "i", "n", "o", "c", "e", "r", "o", "s"], ["r", "a", "c", "o", "o", "n"],
+           ["f", "l", "a", "m", "i", "n", "g", "o"], ["f", "o", "x"], ["g", "o", "a", "t"],
+           ["g", "i", "r", "a", "f", "f", "e"], ["g", "i", "b", "b", "o", "n"], ["g", "e", "c", "k", "o"],
+           ["h", "y", "e", "n", "a"], ["h", "i", "p", "p", "o", "p", "o", "t", "a", "m", "u", "s"],
+           ["h", "o", "r", "s", "e"], ["h", "a", "m", "s", "t", "e", "r"], ["i", "n", "s", "e", "c", "t"],
+           ["i", "m", "p", "a", "l", "a"], ["i", "g", "u", "a", "n", "a"], ["i", "b", "i", "s"],
+           ["j", "a", "c", "k", "a", "l"], ["j", "a", "g", "u", "a", "r"],
+           ["j", "e", "l", "l", "y", "f", "i", "s", "h"], ["k", "a", "n", "g", "a", "r", "o", "o"],
+           ["k", "i", "w", "i"], ["k", "o", "a", "l", "a"], ["k", "i", "l", "l", "e", "r", "w", "h", "a", "l", "e"],
+           ["l", "e", "m", "u", "r"], ["l", "e", "o", "p", "a", "r", "d"], ["l", "l", "a", "m", "a"],
+           ["l", "i", "o", "n"], ["m", "o", "n", "k", "e", "y"], ["m", "o", "u", "s", "e"],
+           ["m", "o", "o", "s", "e"], ["m", "e", "e", "r", "c", "a", "t"], ["n", "u", "m", "b", "a", "t"],
+           ["n", "e", "w", "t"], ["o", "s", "t", "r", "i", "c", "h"], ["o", "t", "t", "e", "r"],
+           ["o", "c", "t", "o", "p", "u", "s"], ["o", "r", "a", "n", "g", "u", "t", "a", "n"],
+           ["p", "e", "n", "g", "u", "i", "n"], ["p", "a", "n", "t", "h", "e", "r"], ["p", "a", "r", "r", "o", "t"],
+           ["p", "i", "g"], ["q", "u", "a", "i", "l"], ["q", "u", "o", "k", "k", "a"], ["q", "u", "o", "l", "l"],
+           ["r", "a", "t"], ["r", "h", "i", "n", "o", "c", "e", "r", "o", "s"], ["r", "a", "c", "o", "o", "n"],
            ["r", "e", "i", "n", "d", "e", "e", "r"], ["r", "a", "b", "b", "i", "t"], ["s", "n", "a", "k", "e"],
            ["s", "q", "u", "i", "r", "r", "e", "l"], ["s", "h", "e", "e", "p"], ["s", "e", "a", "l"],
            ["t", "u", "r", "t", "l", "e"], ["t", "i", "g", "e", "r"], ["t", "u", "r", "k", "e", "y"],
            ["t", "a", "p", "i", "r"], ["u", "n", "i", "c", "o", "r", "n"],
            ["v", "a", "m", "p", "i", "r", "e", "b", "a", "t"], ["v", "u", "l", "t", "u", "r", "e"],
            ["w", "o", "m", "b", "a", "t"], ["w", "a", "l", "r", "u", "s"],
-           ["w", "i", "l", "d", "e", "b", "e", "a", "s", "t"], ["w, a, l, l, a, b, y"], ["y", "a", "k"],
+           ["w", "i", "l", "d", "e", "b", "e", "a", "s", "t"], ["w", "a", "l", "l", "a", "b", "y"], ["y", "a", "k"],
            ["z", "e", "b", "r", "a"]]
+
+
+# ----- removing unwanted characters -----
+photo = "".join(files.split("=")) + " "
+
 # ----- animal character list -----
 scan = []
-# ----- checklist to see if the animal exists within the animals list list -----
-check = []
 
 # ----- catching the entire word -----
-# for i in range(len(photo) - 1):
-#     if not photo[i] == photo[i + 1]:
-#         scan.append(photo[i])
+for i in range(len(photo) - 1):
+    if not photo[i] == photo[i + 1]:
+        scan.append(photo[i])
 
-# ----- catching only unique characters of the word -----
-for i in photo:
-    if i not in scan:
-        scan.append(i)
-# print(scan)
+# ----- checking if theres the same animal on the two lists -----
 
-# ----- searching each scan item with each animal list list item -----
-while A LISTA DE ANIMAIS NÃO TIVER UMA LISTA COM OS MESMOS CARACTERES DA LISTA SCAN:
-    for i in scan: PEGA O ITEM DE SCAN E COMPARA COM A PRIMEIRA LISTA DA DOS ANIMAIS
-                    SE TIVER, PASSA PARA O PRÓXIMO E ASSIM POR DIANTE
-        for item in animals[i]: A LISTA DOS ANIMAIS QUE TIVER  MAIS ITENS IGUAIS AO DA SCAN
-            if i == item:       PARA ISSO TENHO QUE FAZER UMA FUNÇÃO QUE CRIE VARIÁVEIS COM OS "PONTOS" DE CADA LISTA
+unknown = False
 
+for animal in animals:
+    if scan == animal:
+        unknown = False
+        break
+    else:
+        unknown = True
 
+if unknown:
+    print("??")
+else:
+    print(scan)
 
+# ----- checking if theres the same animal on the two lists when the animal comes reverse -----
 
+bicho = []
 
+for i in range(len(scan) - 1, -1, -1):
+    bicho.append(scan[i])
 
-# search = re.search(scan, animals, re.IGNORECASE)
-# print(search)
-
-
-
-# for item in animals:
-#   if i in item:
-#       print(True)
+print(bicho)
 
 
-
-
-# def list_contains(List1, List2):
-#     check = False
-#
-#     # Iterate in the 1st list
-#     for m in List1:
-#
-#         # Iterate in the 2nd list
-#         for n in List2:
-#
-#             # if there is a match
-#             if m == n:
-#                 check = True
-#                 return check
-#
-#     return check
 
 
 
